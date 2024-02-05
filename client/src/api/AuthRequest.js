59b300757db68a986f114a70611d8bc8a5ac0473
Mohammed Nihal <mohammednihal691@gmail.com>
2024-02-05 18:15:26 +0530

    update server url

--- client/src/api/AuthRequest.js
+++ client/src/api/AuthRequest.js
@@ -1,6 +1,7 @@
 import axios from "axios"
 
-const API = axios.create({baseURL:"http://localhost:3000"})
+// const API = axios.create({baseURL:"http://localhost:3000"})
+const API = axios.create({baseURL:"https://banamo-int.vercel.app"})
 
 export const login = (formData )=> API.post('/auth/login',formData)
 export const signup = (formData )=> API.post('/auth/register',formData)
