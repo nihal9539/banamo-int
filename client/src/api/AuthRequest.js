8e967bf6acb0c4b598f74607e4be22844f47f6a5
Mohammed Nihal <mohammednihal691@gmail.com>
2024-02-05 18:34:05 +0530

    update

--- client/src/api/AuthRequest.js
+++ client/src/api/AuthRequest.js
@@ -1,7 +1,17 @@
 import axios from "axios"
 
+
+
+
+// this for local running
 // const API = axios.create({baseURL:"http://localhost:3000"})
+
+// for running on vercel
 const API = axios.create({baseURL:"https://banamo-int.vercel.app"})
+
+
+
+
 
 export const login = (formData )=> API.post('/auth/login',formData)
 export const signup = (formData )=> API.post('/auth/register',formData)
